dd2918f5fae85042fe9534c6a25858e24efbab66
Tim Dresser <tdresser@gmail.com>
2025-04-28 20:22:43 -0400

    Generate Typescript bindings.

--- grassroots-frontend/vite.config.ts
+++ grassroots-frontend/vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({ tsDecorators: true }),
     TanStackRouterVite({
       target: "react",
       autoCodeSplitting: true,
     }),
   ],
+  resolve: {
+    alias: {
+      typeorm: path.resolve(
+        __dirname,
+        "./node_modules/typeorm/typeorm-model-shim",
+      ),
+    },
+  },
 });
--- setup.sh
+++ setup.sh
@@ -13,4 +13,4 @@ popd
 
 # Instead of a traditional repo, we symlink shared code from the backend into the frontend.
 # Details on why are in the Readme.md.
-ln -s "$(pwd)/grassroots-backend/src/grassroots-shared/" "$(pwd)/grassroots-frontend/src"
+ln -sf "$(pwd)/grassroots-backend/src/grassroots-shared/" "$(pwd)/grassroots-frontend/src"
