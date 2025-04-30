bfc884a9ae6beb2a4ca56189df9447f04564b3df
Timothy Dresser <tdresser@gmail.com>
2025-04-30 11:20:21 -0400

    Generate OpenAPI/Typescript fetch bindings.
    Use @nestjs/swagger to generate OpenAPI bindings, following [these instructions](https://docs.nestjs.com/openapi/introduction), followed by [these instructions](https://docs.nestjs.com/openapi/cli-plugin) for setting up the CLI plugin, which automatically adds the appropriate default decorators.
    
    Then use openapi-typescript (setup instructions)[https://openapi-ts.dev/introduction] and openapi-fetch (setup instructions)[https://openapi-ts.dev/openapi-fetch/].
    
    Co-authored-by: Ian Edington <ianedington@gpo.ca>
    Co-authored-by: tdresser <470465+tdresser@users.noreply.github.com>

--- grassroots-backend/src/grassroots-shared/contact.entity.dto.ts
+++ grassroots-backend/src/grassroots-shared/contact.entity.dto.ts
@@ -1,26 +1,43 @@
+import {
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsPhoneNumber,
+  Min,
+} from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 export class CreateContactInDto {
+  @IsEmail()
   email!: string;
+  @IsNotEmpty()
   firstName!: string;
+  @IsNotEmpty()
   lastName!: string;
+  @IsPhoneNumber()
   phoneNumber!: string;
 }
 
 @Entity()
 export class ContactEntityOutDTO {
   @PrimaryGeneratedColumn()
+  @IsInt()
+  @Min(0)
   id!: number;
 
   @Column()
+  @IsEmail()
   email!: string;
 
   @Column()
+  @IsNotEmpty()
   firstName!: string;
 
   @Column()
+  @IsNotEmpty()
   lastName!: string;
 
   @Column()
+  @IsPhoneNumber()
   phoneNumber!: string;
 }
