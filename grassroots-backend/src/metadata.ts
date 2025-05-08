134ae879ba69106fc4c5d8b6c3950c0d8d58896c
Tim Dresser <tdresser@gmail.com>
2025-05-08 11:58:25 -0400

    Actually remove metadata.ts

--- grassroots-backend/src/metadata.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-export default async () => {
-    const t = {
-        ["./app/entities/hello.dto"]: await import("./app/entities/hello.dto"),
-        ["./grassroots-shared/contact.entity.dto"]: await import("./grassroots-shared/contact.entity.dto")
-    };
-    return { "@nestjs/swagger": { "models": [[import("./app/entities/hello.dto"), { "HelloOutDTO": { message: { required: true, type: () => String } } }], [import("./grassroots-shared/contact.entity.dto"), { "CreateContactInDto": { email: { required: true, type: () => String, format: "email" }, firstName: { required: true, type: () => String }, lastName: { required: true, type: () => String }, phoneNumber: { required: true, type: () => String } }, "ContactEntityOutDTO": { id: { required: true, type: () => Number, minimum: 0 }, email: { required: true, type: () => String, format: "email" }, firstName: { required: true, type: () => String }, lastName: { required: true, type: () => String }, phoneNumber: { required: true, type: () => String } } }], [import("./contacts/entities/validationError.dto"), { "ValidationErrorOutDTO": { statusCode: { required: true, type: () => Number }, message: { required: true, type: () => [String] }, error: { required: true, type: () => String } } }], [import("./contacts/entities/contact.entity"), { "Contact": {} }]], "controllers": [[import("./app.controller"), { "AppController": { "getHello": { type: t["./app/entities/hello.dto"].HelloOutDTO } } }], [import("./contacts/contacts.controller"), { "ContactsController": { "create": { type: t["./grassroots-shared/contact.entity.dto"].ContactEntityOutDTO }, "findAll": { type: [t["./grassroots-shared/contact.entity.dto"].ContactEntityOutDTO] }, "findOne": { type: Object } } }]] } };
-};
