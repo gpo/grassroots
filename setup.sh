24fcdd6b1f4318415d16ffa9deb6d058194cfcc1
Ian Edington <ianedington@gpo.ca>
2025-05-18 00:48:04 -0400

    feat(dev): install child packages when root installs

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 npm install
 
-pushd .
-cd grassroots-frontend
-npm install
-popd
-
-pushd .
-cd grassroots-backend
-npm install
-popd
-
 cp .env.development.sample .env.development
 cp .env.test.sample .env.test
 
