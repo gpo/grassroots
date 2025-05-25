bb945a0aafb061fabb18e8975eac60dc7b0a3652
Tim Dresser <tdresser@gmail.com>
2025-05-25 15:17:24 -0400

    non e2e test fixture

--- grassroots-backend/src/contacts/Contacts.service.spec.ts
+++ grassroots-backend/src/contacts/Contacts.service.spec.ts
@@ -1,42 +1,26 @@
 import { ContactsService } from "./Contacts.service";
-import { getTestApp } from "../testing/GetTestApp";
-import { INestApplication } from "@nestjs/common";
 import {
   ContactEntityOutDTO,
   CreateContactInDto,
 } from "../grassroots-shared/Contact.entity.dto";
 import { instanceToPlain, plainToClass } from "class-transformer";
-import { EntityManager } from "@mikro-orm/postgresql";
-//import { QueryRunnerProvider } from "../providers/QueryRunnerProvider";
+import { useTestFixture } from "../testing/Setup";
 
 describe("ContactsService", () => {
-  let service: ContactsService;
-  let app: INestApplication;
-  let entityManager: EntityManager;
-
-  beforeAll(async () => {
-    ({ app } = await getTestApp({}));
-    entityManager = app.get<EntityManager>(EntityManager);
-    service = app.get<ContactsService>(ContactsService);
+  const getFixture = useTestFixture({
+    providers: [ContactsService],
   });
 
-  afterAll(async () => {
-    await app.close();
-  });
-
-  beforeEach(async () => {
-    await entityManager.begin();
-  });
-
-  afterEach(async () => {
-    await entityManager.rollback();
-  });
+  function getService(): ContactsService {
+    return getFixture().app.get<ContactsService>(ContactsService);
+  }
 
   it("should be defined", () => {
-    expect(service).toBeDefined();
+    expect(getService()).toBeDefined();
   });
 
   it("should create and return a contact", async () => {
+    const service = getService();
     const contact: CreateContactInDto = {
       email: "test@test.com",
       firstName: "Test",
@@ -56,7 +40,7 @@ describe("ContactsService", () => {
   });
 
   it("should have no entries in the test database", async () => {
-    const allContacts = await service.findAll();
+    const allContacts = await getService().findAll();
     expect(allContacts.length).toEqual(0);
   });
 });
---
+++ grassroots-backend/src/testing/Setup.ts
@@ -0,0 +1,47 @@
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { getTestApp, TestSpecificDependencies } from "./GetTestApp";
+import { EntityManager } from "@mikro-orm/core";
+import { MikroORM } from "@mikro-orm/postgresql";
+
+export class TestFixture {
+  app: NestExpressApplication;
+
+  constructor(props: { app: NestExpressApplication }) {
+    this.app = props.app;
+  }
+}
+
+// Inspired by https://mattburke.dev/using-test-hooks-for-shared-fixtures/
+export function useTestFixture(
+  dependencies: TestSpecificDependencies,
+): () => TestFixture {
+  let fixture: TestFixture | undefined;
+
+  beforeAll(async () => {
+    const { app } = await getTestApp(dependencies);
+    const orm = app.get<MikroORM>(MikroORM);
+    const generator = orm.getSchemaGenerator();
+    await generator.dropSchema();
+    await generator.createSchema();
+    fixture = new TestFixture({ app });
+  });
+
+  afterAll(async () => {
+    await fixture?.app.close();
+  });
+
+  beforeEach(async () => {
+    await fixture?.app.get<EntityManager>(EntityManager).begin();
+  });
+
+  afterEach(async () => {
+    await fixture?.app.get<EntityManager>(EntityManager).rollback();
+  });
+
+  return () => {
+    if (fixture === undefined) {
+      throw new Error("Failed to initialize fixture");
+    }
+    return fixture;
+  };
+}
