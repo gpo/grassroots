ef199c7e7e35d383bbde4c3e6e3823cd3d73d78c
Ian Edington <ianedington@gpo.ca>
2025-05-26 21:54:26 -0400

    dev: use editorconfig to lint all other files
    Tab vs space, white space and final new line diffs have been coming up in PRs.
    By fixing all the files prettier doesn't fix, we can remove these diffs.

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "!(*.ts|*.tsx)": "prettier --write --ignore-unknown",
+  "!(*.ts|*.tsx)": ["prettier --write --ignore-unknown", "eclint fix"],
   "*.{ts,tsx}": ["eslint --fix", "prettier --write"],
   "*{package.json,package-lock.json}": () => ["syncpack list-mismatches"],
 };
