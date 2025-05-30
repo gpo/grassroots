416eb5741e2bd9714316215b9fe1293cb3f5f63f
Tim Dresser <tdresser@gmail.com>
2025-05-30 12:35:51 -0400

    Move login to auth controller

--- grassroots-backend/src/App.controller.ts
+++ grassroots-backend/src/App.controller.ts
@@ -1,10 +1,6 @@
-import { Controller, Get, Post, UseGuards, Request } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AppService } from "./App.service";
 import { HelloOutDTO } from "./app/entities/Hello.dto";
-import { UserEntity } from "./grassroots-shared/User.entity";
-import { DefaultAuthGuard } from "./auth/DefaultAuth.guard";
-import type { GrassrootsRequest } from "./types/GrassrootsRequest";
-import { ApiBody } from "@nestjs/swagger";
 import { AuthService } from "./auth/Auth.service";
 
 @Controller()
@@ -17,24 +13,5 @@ export class AppController {
   @Get()
   getHello(): HelloOutDTO {
     return this.appService.getHello();
-  }
-
-  @UseGuards(DefaultAuthGuard)
-  @ApiBody({ type: UserEntity })
-  @Post("auth/login")
-  async login(
-    @Request() req: GrassrootsRequest,
-  ): Promise<UserEntity | undefined> {
-    return new Promise((resolve) => {
-      if (!req.user) {
-        throw new Error("No user found for login.");
-      }
-      req.login(req.user, (err) => {
-        if (err) {
-          throw err;
-        }
-        resolve(req.user);
-      });
-    });
   }
 }
