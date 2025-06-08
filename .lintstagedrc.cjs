d0fc79c28250b1f0826b036a1c787867231ea482
Mark <markwong167@gmail.com>
2025-06-08 01:35:39 -0400

    Migrated eclint to editorconfig-checker
    Max line length is already being wrapped by prettier (where the default is 80). editorconfig might not even be necessary.

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "!(*.ts|*.tsx)": ["eclint fix", "prettier --write --ignore-unknown"],
+  "!(*.ts|*.tsx)": ["prettier --write --ignore-unknown", "ec"],
   "*.{ts,tsx}": ["eslint --fix", "prettier --write"],
   "*{package.json,package-lock.json}": () => ["syncpack list-mismatches"],
 };
