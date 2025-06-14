0af55fba4bb713a7e963d7055b0c136496bc3ce1
Tim Dresser <tdresser@gmail.com>
2025-06-14 15:04:52 -0400

    Cleanup

--- grassroots-backend/src/testing/GlobalSetup.ts
+++ grassroots-backend/src/testing/GlobalSetup.ts
@@ -1,14 +1,6 @@
 import mikroORMConfig from "../mikro-orm.config";
 import { MikroORM } from "@mikro-orm/core";
 
-import dotenvFlow from "dotenv-flow";
-import { getEnvFilePaths } from "../GetEnvFilePaths";
-
-dotenvFlow.config({
-  // Reversed so that earlier files take priority, to align with the ConfigModule.
-  files: getEnvFilePaths().reverse(),
-});
-
 export default async function setup(): Promise<void> {
   const orm = await MikroORM.init(mikroORMConfig);
   const generator = orm.getSchemaGenerator();
