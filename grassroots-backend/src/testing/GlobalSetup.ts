aa9834217feeaa644be8a5750fba526738e92cb5
Timothy Dresser <tdresser@gmail.com>
2025-06-14 23:26:00 -0400

    Hardcode to use test env files when under test (#88)

--- grassroots-backend/src/testing/GlobalSetup.ts
+++ grassroots-backend/src/testing/GlobalSetup.ts
@@ -1,8 +1,8 @@
-import defineConfig from "../mikro-orm.config";
+import mikroORMConfig from "../mikro-orm.config";
 import { MikroORM } from "@mikro-orm/core";
 
 export default async function setup(): Promise<void> {
-  const orm = await MikroORM.init(defineConfig);
+  const orm = await MikroORM.init(mikroORMConfig);
   const generator = orm.getSchemaGenerator();
   await generator.dropSchema();
   await generator.createSchema();
