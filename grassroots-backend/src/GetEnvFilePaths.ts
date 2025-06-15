218ab5efd952d4cdda28d0b953a39986e8e8a8ec
Tim Dresser <tdresser@gmail.com>
2025-06-15 15:37:20 -0400

    Debug logs.

--- grassroots-backend/src/GetEnvFilePaths.ts
+++ grassroots-backend/src/GetEnvFilePaths.ts
@@ -1,5 +1,7 @@
 // Earlier files take priority.
 export function getEnvFilePaths(): string[] {
+  console.log("github actions?", process.env.GITHUB_ACTIONS);
+  console.log("mode?", process.env.MODE);
   if (process.env.GITHUB_ACTIONS == "true") {
     return ["../.env.test.ci", "../.env/test"];
   }
