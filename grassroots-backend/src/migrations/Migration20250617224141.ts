60af5bc9af2d6a35efcf591268012dbe80ed8b7f
Tim Dresser <tdresser@gmail.com>
2025-06-17 18:42:43 -0400

    Addressing missing migration from last patch

---
+++ grassroots-backend/src/migrations/Migration20250617224141.ts
@@ -0,0 +1,13 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20250617224141 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql(`alter table "user_entity" add column "emails" jsonb null;`);
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`alter table "user_entity" drop column "emails";`);
+  }
+
+}
