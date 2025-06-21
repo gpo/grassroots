6a1da00be287a94670ad3120542de269e770dbb3
Ian Edington <ianedington@gpo.ca>
2025-06-21 00:51:21 -0400

    generate when there is no file

---
+++ grassroots-backend/src/metadata.stub.ts
@@ -0,0 +1,8 @@
+/* eslint-disable check-file/filename-naming-convention */
+/**
+ * This is an auto-generated file, overwritten during the build process.
+ * It is a stub file to satisfy the TypeScript compiler.
+ */
+export default (): Promise<Record<string, unknown>> => {
+  return Promise.resolve({});
+};
