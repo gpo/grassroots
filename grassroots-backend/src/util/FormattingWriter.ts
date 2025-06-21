6d483b6a8c56f7fc47ff04739a1e781adae7ddb4
Tim Dresser <tdresser@gmail.com>
2025-06-21 20:06:32 +0000

    Format generated files

---
+++ grassroots-backend/src/util/FormattingWriter.ts
@@ -0,0 +1,31 @@
+import { readFile, writeFile } from "fs/promises";
+import * as prettier from "prettier";
+
+export interface WriteFormattedResult {
+  noChange: boolean;
+}
+
+let prettierOptions: prettier.Options | null = null;
+
+export async function writeFormatted(options: {
+  filePath: string;
+  onlyIfChanged?: boolean;
+  text: string;
+}): Promise<WriteFormattedResult> {
+  prettierOptions ??= await prettier.resolveConfig("../.prettier.rc");
+
+  const formatted = await prettier.format(options.text, {
+    ...prettierOptions,
+    filepath: options.filePath,
+  });
+
+  if (options.onlyIfChanged === true) {
+    const oldText = await readFile(options.filePath, "utf8");
+    if (oldText == formatted) {
+      return { noChange: true };
+    }
+  }
+
+  await writeFile(options.filePath, formatted);
+  return { noChange: false };
+}
