1a82891bd33705821934f05e83ff0e8d54585892
Ian Edington <ianedington@gpo.ca>
2025-06-21 22:22:26 -0400

    delete metadata.ts

---
+++ grassroots-backend/src/metadata.stub.ts
@@ -0,0 +1,7 @@
+/* eslint-disable check-file/filename-naming-convention */
+/**
+ * It is a stub file to satisfy the TypeScript compiler.
+ */
+export default (): Promise<Record<string, unknown>> => {
+  return Promise.resolve({});
+};
