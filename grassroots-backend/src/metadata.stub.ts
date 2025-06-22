ed905122af3190a92a180f744581cdc68e97cef0
Ian Edington <ianedington@gpo.ca>
2025-06-21 22:27:40 -0400

    remove stub since we can just use the formatted version

--- grassroots-backend/src/metadata.stub.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable check-file/filename-naming-convention */
-/**
- * It is a stub file to satisfy the TypeScript compiler.
- */
-export default (): Promise<Record<string, unknown>> => {
-  return Promise.resolve({});
-};
