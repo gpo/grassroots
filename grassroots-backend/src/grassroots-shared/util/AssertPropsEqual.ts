ee4a3e278b4160a70fbe6acbae340fdbc4cefd69
Timothy Dresser <tdresser@gmail.com>
2025-06-27 14:36:46 -0400

    Fix AssertPropsEqual with optional fields. (#119)

--- grassroots-backend/src/grassroots-shared/util/AssertPropsEqual.ts
+++ grassroots-backend/src/grassroots-shared/util/AssertPropsEqual.ts
@@ -3,9 +3,12 @@ import { PropsOf } from "./PropsOf";
 // To use:
 // export const check: AssertPropsEqual<A, B> = true;
 
+// From https://dev.to/tylim88/typescript-one-import-type-equality-check-24kj .
+
 export type AssertPropsEqual<T, U> =
-  PropsOf<T> extends PropsOf<U>
-    ? PropsOf<U> extends PropsOf<T>
-      ? true
-      : never
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
+  (<G>() => G extends PropsOf<T> ? 1 : 2) extends <G>() => G extends PropsOf<U>
+    ? 1
+    : 2
+    ? true
     : never;
