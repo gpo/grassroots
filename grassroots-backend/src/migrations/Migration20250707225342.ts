21b7f4367d1bfcf5547731412a0118f44257e257
Mark <markwong167@gmail.com>
2025-07-07 19:34:34 -0400

    add migration

---
+++ grassroots-backend/src/migrations/Migration20250707225342.ts
@@ -0,0 +1,19 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20250707225342 extends Migration {
+
+  override async up(): Promise<void> {
+    this.addSql(`create table "contact_entity" ("id" serial primary key, "email" varchar(255) not null, "first_name" varchar(255) not null, "last_name" varchar(255) not null, "phone_number" varchar(255) not null);`);
+    this.addSql(`alter table "contact_entity" add constraint "contact_entity_email_unique" unique ("email");`);
+
+    this.addSql(`drop table if exists "contact_entity_out_dto" cascade;`);
+  }
+
+  override async down(): Promise<void> {
+    this.addSql(`create table "contact_entity_out_dto" ("id" serial primary key, "email" varchar(255) not null, "first_name" varchar(255) not null, "last_name" varchar(255) not null, "phone_number" varchar(255) not null);`);
+    this.addSql(`alter table "contact_entity_out_dto" add constraint "contact_entity_out_dto_email_unique" unique ("email");`);
+
+    this.addSql(`drop table if exists "contact_entity" cascade;`);
+  }
+
+}
