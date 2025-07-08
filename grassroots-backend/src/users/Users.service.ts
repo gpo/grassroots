7acf99a58baf94f236f1aab14f6aa7f056be9394
Tim Dresser <tdresser@gmail.com>
2025-07-08 17:25:16 +0000

    Fixed tests

--- grassroots-backend/src/users/Users.service.ts
+++ grassroots-backend/src/users/Users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { UserEntity } from "./User.entity";
 import { EntityManager, EntityRepository } from "@mikro-orm/core";
-import { PropsOf } from "../grassroots-shared/util/PropsOf";
+import { UserDTO } from "../grassroots-shared/User.dto";
 
 @Injectable()
 export class UsersService {
@@ -9,27 +9,21 @@ export class UsersService {
   constructor(private readonly entityManager: EntityManager) {
     this.repo = entityManager.getRepository<UserEntity>(UserEntity);
   }
-  async findOrCreate(
-    user: PropsOf<UserEntity>,
-  ): Promise<UserEntity | undefined> {
+  async findOrCreate(user: UserDTO): Promise<UserDTO | undefined> {
     const existing = await this.repo.findOne({ id: user.id });
     if (existing !== null) {
       return existing;
     }
-    return this.repo.create(user);
-  }
-
-  async createOrFindOne(user: PropsOf<UserEntity>): Promise<UserEntity | null> {
-    let result = await this.repo.findOne(user);
-    if (result) {
-      return result;
-    }
-    result = this.repo.create(user);
+    const result = this.repo.create(user);
     await this.entityManager.flush();
     return result;
   }
 
-  async findAll(): Promise<UserEntity[]> {
+  async findOneById(id: string): Promise<UserDTO | null> {
+    return await this.repo.findOne({ id });
+  }
+
+  async findAll(): Promise<UserDTO[]> {
     return await this.repo.find({});
   }
 }
