66911d5292625cdf4237f8baf57150afb1e79cfa
Tim Dresser <tdresser@gmail.com>
2025-07-10 15:25:05 +0000

    Currently doing collections wrong

--- grassroots-backend/src/users/User.entity.ts
+++ grassroots-backend/src/users/User.entity.ts
@@ -1,20 +1,25 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, OneToMany, PrimaryKey, Property } from "@mikro-orm/core";
 import { UserDTO } from "../grassroots-shared/User.dto";
-import { AssertPropsEqual } from "../grassroots-shared/util/AssertPropsEqual";
 import { PropsOf } from "../grassroots-shared/util/PropsOf";
+import { UserRoleEntity } from "./UserRole.entity";
 
 @Entity()
-export class UserEntity implements PropsOf<UserDTO> {
+export class UserEntity implements Omit<PropsOf<UserDTO>, "roles"> {
   @PrimaryKey()
   id!: string;
+
   @Property({ type: "json", nullable: true })
   emails?: string[];
+
   @Property({ nullable: true })
   firstName?: string;
+
   @Property({ nullable: true })
   lastName?: string;
+
   @Property({ nullable: true })
   displayName?: string;
-}
 
-export const check: AssertPropsEqual<UserDTO, UserEntity> = true;
+  @OneToMany(() => UserRoleEntity, (e) => e.user)
+  roles!: UserRoleEntity[];
+}
