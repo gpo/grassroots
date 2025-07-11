b0a45553ac0d8945c42da4206d087fa919b565a8
Tim Dresser <tdresser@gmail.com>
2025-07-11 16:46:15 +0000

    Auth should never use Service directly I think..

--- grassroots-backend/src/users/Users.e2e-spec.ts
+++ grassroots-backend/src/users/Users.e2e-spec.ts
@@ -1,6 +1,7 @@
 import { assert, describe, expect, it } from "vitest";
 import { useE2ETestFixture } from "../testing/E2eSetup";
 import { UsersModule } from "./Users.module";
+import { createOrganizationTree } from "../testing/testHelpers/CreateOrganizationTree";
 
 describe("Users (e2e)", () => {
   const getFixture = useE2ETestFixture({
@@ -9,6 +10,13 @@ describe("Users (e2e)", () => {
 
   it("should return valid permissions", async () => {
     const f = getFixture();
+    await createOrganizationTree(f, {
+      name: "root",
+      children: [
+        { name: "A", children: [{ name: "B", children: [{ name: "C" }] }] },
+        { name: "Unrelated" },
+      ],
+    });
     const { data } = await f.grassrootsAPI.GET("/roles");
     assert(data !== undefined);
     expect(data.length).toEqual(4);
