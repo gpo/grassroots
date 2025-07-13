22b7d95e28b7fcbe1e9664ffb1cf82dc65b128dd
Tim Dresser <tdresser@gmail.com>
2025-07-13 16:18:03 +0000

    Classname case and ending

--- eslint_rules/src/rules/dto-and-entity-style.ts
+++ eslint_rules/src/rules/dto-and-entity-style.ts
@@ -3,7 +3,7 @@ import { AST_NODE_TYPES, TSESTree } from "@typescript-eslint/utils";
 import { createRule } from "../utils.js";
 import { RuleContext, RuleListener } from "@typescript-eslint/utils/ts-eslint";
 
-type MessageIds = "definiteOrOptional";
+type MessageIds = "definiteOrOptional" | "classNameRules";
 
 export const rule = createRule({
   create(context: Readonly<RuleContext<MessageIds, []>>): RuleListener {
@@ -13,8 +13,15 @@ export const rule = createRule({
         if (name === undefined) {
           return;
         }
-        if (!/(DTO|Entity)/.exec(name)) {
+        if (!/(DTO|Entity)/i.exec(name)) {
           return;
+        }
+        // We need to match case sensitively, and DTO / Entity should be at the end of the class name.
+        if (!/(DTO|Entity)$/.exec(name)) {
+          context.report({
+            messageId: "classNameRules",
+            node,
+          });
         }
         for (const element of node.body.body) {
           if (element.type !== AST_NODE_TYPES.PropertyDefinition) {
@@ -40,6 +47,7 @@ export const rule = createRule({
       definiteOrOptional: `Properties must be definite (with a ! suffix) or optional (with a ? suffix).
         Since these objects are constructed via class-transformer, they won't be initialized in the constructor.
         This lets typescript know that it can assume they're present despite this.`,
+      classNameRules: `DTOs and Entity class names must end in DTO or Entity, case sensitive`,
     },
     type: "suggestion",
     schema: [],
