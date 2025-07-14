3f808fa0ac74c08386b03a3e091ae2d748d42daf
Tim Dresser <tdresser@gmail.com>
2025-07-14 18:08:50 +0000

    Fix type error

--- grassroots-backend/src/types/GlobalAuth.d.ts
+++ grassroots-backend/src/types/GlobalAuth.d.ts
@@ -1,8 +1,8 @@
-import { UserEntity } from "../users/User.entity";
+import { UserDTO } from "../grassroots-shared/User.dto";
 
 declare global {
   namespace Express {
     // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    interface User extends UserEntity {}
+    interface User extends UserDTO {}
   }
 }
