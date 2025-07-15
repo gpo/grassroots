8e80447dfc664fa0cbaa8555c1a16085d25c91de
Tim Dresser <tdresser@gmail.com>
2025-07-15 15:52:22 +0000

    Fixing type errors

--- grassroots-backend/src/users/Users.controller.ts
+++ grassroots-backend/src/users/Users.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from "@nestjs/common";
-import { UserDTO } from "../grassroots-shared/User.dto";
+import { UsersDTO } from "../grassroots-shared/User.dto";
 import { UsersService } from "../users/Users.service";
 
 @Controller("users")
@@ -7,7 +7,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findAll(): Promise<UserDTO[]> {
-    return this.usersService.findAll();
+  findAll(): Promise<UsersDTO> {
+    return { users: this.usersService.findAll() };
   }
 }
