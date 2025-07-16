8e2de6305ec1302c2412d933dc96074c5eae8b84
Tim Dresser <tdresser@gmail.com>
2025-07-16 19:35:43 +0000

    Rebase

--- eslint_rules/src/rules/EntityUse.test.ts
+++ eslint_rules/src/rules/EntityUse.test.ts
@@ -1,5 +1,5 @@
 import { rule } from "./EntityUse.js";
-import { createRuleTester } from "../test-utils.js";
+import { createRuleTester } from "../TestUtils.js";
 
 const ruleTester = createRuleTester();
 
