84b60ad8a29147d720658bdb89870ee4996aeaae
Mark <markwong167@gmail.com>
2025-07-20 19:42:36 -0400

    the probable culprit of the re-installed package-locks

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   "!(*.ts|*.tsx)": ["prettier --write --ignore-unknown"],
   "*.{ts,tsx}": ["eslint --fix", "prettier --write"],
-  "*{package.json,package-lock.json}": () => ["syncpack list-mismatches"],
+  "*{package.json,pnpm-lock.yaml}": () => ["syncpack list-mismatches"],
 };
