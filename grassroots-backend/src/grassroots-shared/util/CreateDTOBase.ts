2f6d04e4b0714d33ee8d956b5815a82220dd9959
Tim Dresser <tdresser@gmail.com>
2025-07-24 18:50:43 +0000

    Fix include

--- grassroots-backend/src/grassroots-shared/util/CreateDTOBase.ts
+++ grassroots-backend/src/grassroots-shared/util/CreateDTOBase.ts
@@ -24,6 +24,7 @@ only to convince typescript that these types shouldn't be castable to one anothe
 */
 
 import { plainToInstance } from "class-transformer";
+import { PropsOf } from "./TypeUtils";
 
 export interface FetchResponse<T, E> {
   data?: T;
