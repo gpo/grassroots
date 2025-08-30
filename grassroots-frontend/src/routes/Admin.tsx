738875df5febefbfe13a682c01f24adb4ea85f49
Mark <markwong167@gmail.com>
2025-08-30 02:13:50 -0400

    unsaved change

--- grassroots-frontend/src/routes/Admin.tsx
+++ grassroots-frontend/src/routes/Admin.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { AdminPage } from "../components/admin/AdminPage";
+import { AdminPage } from "../areas/admin/AdminPage";
 
 export const Route = createFileRoute("/Admin")({
   component: AdminPage,
