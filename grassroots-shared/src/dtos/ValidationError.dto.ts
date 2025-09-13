d5c0d578ddec8e81354363b6fb3eb7ff3e39de72
Tim Dresser <tdresser@gmail.com>
2025-09-13 23:46:09 +0000

    Working

--- grassroots-shared/src/dtos/ValidationError.dto.ts
+++ grassroots-shared/src/dtos/ValidationError.dto.ts
@@ -1,4 +1,4 @@
-import { createDTOBase } from "grassroots-shared/util/CreateDTOBase";
+import { createDTOBase } from "../util/CreateDTOBase.js";
 
 export class ValidationErrorOutDTO extends createDTOBase("ValidationErrorOut") {
   statusCode!: number;
