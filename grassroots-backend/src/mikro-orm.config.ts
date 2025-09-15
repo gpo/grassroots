11d0ebcbee420efe01451b055a8c952a6dcac149
Tim Dresser <tdresser@gmail.com>
2025-09-15 17:16:56 +0000

    Fix and simplify mikro-orm

--- grassroots-backend/src/mikro-orm.config.ts
+++ grassroots-backend/src/mikro-orm.config.ts
@@ -21,7 +21,4 @@ export default defineConfig({
   password: process.env.POSTGRES_PASSWORD,
   dbName: process.env.POSTGRES_DATABASE,
   debug: true,
-  migrations: {
-    path: "./dist-build/migrations",
-  },
 });
