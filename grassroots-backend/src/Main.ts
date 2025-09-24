1ba73bb58716e9a808864a8ca695bc93897fcf67
Tim Dresser <tdresser@gmail.com>
2025-09-24 13:39:07 +0000

    BuildMetadata in separate process.

--- grassroots-backend/src/Main.ts
+++ grassroots-backend/src/Main.ts
@@ -15,20 +15,14 @@ import { writeFormatted } from "./util/FormattingWriter.js";
 // Needs to show up somewhere for decorators to work.
 import "reflect-metadata";
 import { ValidationErrorOutDTO } from "grassroots-shared/dtos/ValidationError.dto";
-import { readFile } from "fs/promises";
-import { watch } from "chokidar";
 import { LAST_DEPENDENCY_UPDATE_TIME } from "./util/LastDependencyUpdateTime.js";
 import { watchDeps } from "./build/WatchDeps.js";
-import { argv, exit } from "process";
-import { buildMetadata } from "./build/BuildMetadata.js";
+import { argv } from "process";
 import metadata from "./FormattedMetadata.gen.js";
+import { mkdir } from "fs/promises";
+import { dirname } from "path";
 
 const watching = argv.includes("--watch") || argv.includes("-w");
-const skipMetadata = argv.includes("--skip-metadata");
-
-if (!skipMetadata) {
-  buildMetadata(watching);
-}
 
 // If grassroots-shared changes, rebuild it, and update LAST_DEPENDENCY_UPDATE_TIME to trigger a reload.
 if (watching) {
@@ -38,8 +32,6 @@ void LAST_DEPENDENCY_UPDATE_TIME;
 
 const openAPISchemaPath = "./openAPI.json";
 const openAPITSSchemaPath = "../openapi-paths/src/OpenAPI.gen.ts";
-const METADATA_PATH = "./src/metadata.ts";
-const FIXED_METADATA_PATH = "./src/FormattedMetadata.gen.ts";
 
 async function writeOpenAPI(app: NestExpressApplication): Promise<void> {
   performance.mark("writeOpenAPI");
@@ -49,7 +41,6 @@ async function writeOpenAPI(app: NestExpressApplication): Promise<void> {
     .setVersion("0.0")
     .build();
 
-  await fixMetadataPaths();
   await SwaggerModule.loadPluginMetadata(metadata);
 
   const openAPI = SwaggerModule.createDocument(app, config, {
@@ -73,6 +64,7 @@ async function writeOpenAPI(app: NestExpressApplication): Promise<void> {
     console.log("Updating OpenAPI Schema TS bindings");
     const ast = await openapiTS(openAPIStr);
     const contents = astToString(ast);
+    await mkdir(dirname(openAPISchemaPath), { recursive: true });
     await writeFormatted({
       filePath: openAPITSSchemaPath,
       text: contents,
@@ -106,27 +98,6 @@ async function createMikroORMMigration(
   }
 }
 
-// The nestjs metadata generator produces relative paths, but we need absolute paths.
-async function fixMetadataPaths(): Promise<void> {
-  let metadata = await readFile(METADATA_PATH, "utf8");
-
-  const importRegex =
-    /import\("(..\/..\/)?grassroots-shared\/src\/([^"]*)\.js"\)/g;
-  metadata = metadata.replaceAll(importRegex, 'import("grassroots-shared/$2")');
-
-  console.log("CHECKING");
-  const changed = await writeFormatted({
-    filePath: FIXED_METADATA_PATH,
-    text: metadata,
-    onlyIfChanged: true,
-  });
-
-  if (!changed.noChange) {
-    console.log("Need to rerun to pick up new metadata.");
-    exit(1);
-  }
-}
-
 async function bootstrap(port: number): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await listenAndConfigureApp(app, port);
@@ -148,15 +119,7 @@ async function bootstrap(port: number): Promise<void> {
 
   if (process.argv.includes("--gen-files-only")) {
     await app.close();
-    return;
   }
-
-  // Whenever we update the metadata.ts, we need to create a version with the paths fixed.
-  watch(METADATA_PATH, { ignoreInitial: true }).on("all", () => {
-    void (async (): Promise<void> => {
-      await fixMetadataPaths();
-    })();
-  });
 }
 
 const port = process.env.PORT !== undefined ? parseInt(process.env.PORT) : 3000;
